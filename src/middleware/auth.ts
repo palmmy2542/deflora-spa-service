edbda2288131dc05c714304ace017e4dda1367e7
Pubeth Changprung <palmch2542@gmail.com>
2025-09-22 02:02:36 +0700

    feat: apply auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -8,12 +8,12 @@ export async function authenticate(
   next: NextFunction
 ) {
   try {
-    // const authHeader = req.headers.authorization || "";
-    // const token = authHeader.startsWith("Bearer ") ? authHeader.slice(7) : null;
-    // if (!token) return res.status(401).json({ error: "Missing token" });
+    const authHeader = req.headers.authorization || "";
+    const token = authHeader.startsWith("Bearer ") ? authHeader.slice(7) : null;
+    if (!token) return res.status(401).json({ error: "Missing token" });
 
-    // const decoded = await authAdmin.verifyIdToken(token);
-    // (req as any).user = decoded; // contains uid, email, and custom claims
+    const decoded = await authAdmin.verifyIdToken(token);
+    (req as any).user = decoded; // contains uid, email, and custom claims
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
